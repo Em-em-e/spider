ef1f6074eb4ab20a5889f4d840465a7970dd2b2c
frank.li <frank.l.li@gsk.com>
2017-06-09 17:56:58 +0800

    监听dom测试

--- WebContent/WEB-INF/chrome extend program/income.js
+++ WebContent/WEB-INF/chrome extend program/income.js
@@ -4,18 +4,22 @@
 	$.get("http://123.206.231.254/spider/getRate",function(data){
 		var rate=parseFloat(data);
 		
+		
 		//头条
 		var a1=$(".pgc-dashboard-column .pgc-dashboard-primary").eq(0).text("dfsdf");
 		console.log(a1.eq(0));
 
 		//百度百家
-		setTimeout(function(){
+//		setTimeout(function(){
 			//首页  昨日收益
 			var a2=$(".index-top-statistics-number:eq(0)");
+			title.bind('DOMNodeInserted',function(e) {
+			    alert('element now contains: '+ $(e.target).html());
+			});
 			console.log(a2);
 			if(!isNaN(parseFloat(a2.text())))
 				a2.text(fmoney(a2.text()*rate,2));
-		}, 1000);
+//		}, 1000);
 		
 		var income1=$(".num.money:eq(0)");
 		var income2=$(".num.money:eq(1)");
